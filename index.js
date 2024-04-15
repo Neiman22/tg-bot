8c00a2a2ca2598d67e95d9c8730db298e562202e
Neiman22 <pushkarev_av@inbox.ru>
2024-04-15 15:59:48 +0300

    first message

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+import TelegramApi from 'node-telegram-bot-api';
+
 const token = '7074560451:AAFW01fuawQPiP5jaxu02lwcRcQSQMTphEU';
+
+const bot = new TelegramApi(token, { polling: true });
+
+bot.on('message', msg => {
+  const text = msg.text;
+  const chatId = msg.chat.id;
+
+  if (text === '/start') {
+    bot.sendMessage(chatId, `Добро пожаловать`);
+  }
+
+  if (text === '/info') bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`);
+  bot.sendMessage(chatId, `Ты написал мне: ${text}`);
+})
