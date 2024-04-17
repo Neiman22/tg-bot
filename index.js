e2bf0e888c1a504e70027aa7d3065f8407999c54
Neiman22 <pushkarev_av@inbox.ru>
2024-04-17 16:03:25 +0300

    feat: split files

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
 import TelegramApi from 'node-telegram-bot-api';
+import { gameOptions, againOptions } from './options.js';
 
 const token = '7074560451:AAFW01fuawQPiP5jaxu02lwcRcQSQMTphEU';
-
 const bot = new TelegramApi(token, { polling: true });
-
 const chats = {};
-
-const gameOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [{text: '1', callback_data: '1'}, {text: '2', callback_data: '2'}, {text: '3', callback_data: '3'}],
-      [{text: '4', callback_data: '4'}, {text: '5', callback_data: '5'}, {text: '6', callback_data: '6'}],
-      [{text: '7', callback_data: '7'}, {text: '8', callback_data: '8'}, {text: '9', callback_data: '9'}],
-      [{text: '0', callback_data: '0'}]
-    ]
-  })
-};
-
-const againOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [{text: 'Еще раз', callback_data: '/again'}]
-    ]
-  })
-};
 
 const startGame = async (chatId) => {
   const randomNumber = Math.floor(Math.random() * 10);
@@ -42,17 +22,17 @@ const start = async () => {
     const text = msg.text;
     const chatId = msg.chat.id;
   
-    if (text === '/start') {
-      await bot.sendMessage(chatId, `Добро пожаловать`);
-      return bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/8a1/9aa/8a19aab4-98c0-37cb-a3d4-491cb94d7e12/2.webp');
+    switch (text) {
+      case '/start':
+        await bot.sendMessage(chatId, `Добро пожаловать`);
+        return bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/8a1/9aa/8a19aab4-98c0-37cb-a3d4-491cb94d7e12/2.webp');
+      case '/info':
+        return bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`);
+      case '/game':
+        return startGame(chatId);
+      default:
+        return bot.sendMessage(chatId, `Неверная команда`);
     }
-    if (text === '/info') {
-      return bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`);
-    }
-    if (text === '/game') {
-      return startGame(chatId);
-    }
-    return bot.sendMessage(chatId, `Неверная команда`);
   })
 
   bot.on('callback_query', async msg => {
